9960f60fcdc0e365b1b45025a137d8ce1578b981
Wellington Lima <wellingtonlima.jt@gmail.com>
2024-07-23 20:14:16 -0300

    Início - Aula 01

---
+++ Aula-01/script1.js
@@ -0,0 +1,4 @@
+let nome = "Wellington Lima"
+let idade = 26
+
+console.log("Seu nome é " + nome + " e sua idade é " + idade)
---
+++ Aula-01/script2.js
@@ -0,0 +1,5 @@
+let rlSync = require("readline-sync")
+
+let nome = rlSync.question("Qual é o seu nome?\n")
+
+console.log(`Olá, ${nome}`)
